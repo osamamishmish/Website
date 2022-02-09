bd83e398408e79caac94571d6ef5f4e4f41d1a9e
osamamishmish <osama.mishmish16@gmail.com>
2022-02-10 01:38:28 +0200

    add the project files

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".navbar-toggler-icon").on("click",function(){
+        $(".navbar-collapse").addClass("d-flex justify-content-end");
+        $(".nav-item").css("fontSize","12px");
+    })
+})
